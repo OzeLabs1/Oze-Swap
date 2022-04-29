dc41f39fad4a83bbed80c1083d080626c71de5c9
THEDREAMCREATION <102424111+THEDREAMCREATION@users.noreply.github.com>
2022-04-29 13:27:13 +0530

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,6 @@
 import React from "react";
 import "./App.less";
 import { Routes } from "./routes";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-    getPhantomWallet,
-    getSlopeWallet,
-    getSolflareWallet,
-    getSolflareWebWallet,
-    getSolletWallet,
-    getSolletExtensionWallet,
-    getSolongWallet,
-    getLedgerWallet,
-    getSafePalWallet,
-} from "@solana/wallet-adapter-wallets";
-
-import {
-  WalletModalProvider
-} from '@solana/wallet-adapter-react-ui';
-
-const App = () => {
-  // Custom RPC endpoint.
-const endpoint = useMemo(() => clusterApiUrl(network), []);
-
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-  // Only the wallets you configure here will be compiled into your application, and only the dependencies
-  // of wallets that your users connect to will be loaded.
-  const wallets = useMemo(
-      () => [
-          getPhantomWallet(),
-          getSlopeWallet(),
-          getSolflareWallet(),
-          getSolflareWebWallet(),
-          getSolletWallet({ network }),
-          getSolletExtensionWallet({ network }),
-          getSolongWallet(),
-          getLedgerWallet(),
-          getSafePalWallet(),
-      ],
-      []
-  );
 function App() {
   return (
     <div className="App">
