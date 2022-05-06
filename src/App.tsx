47394789ab43e38dc77206521fe791f78385d349
THEDREAMCREATION <102424111+THEDREAMCREATION@users.noreply.github.com>
2022-05-06 18:13:33 +0530

    master

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,9 @@ import { Routes } from "./routes";
 function App() {
   return (
     <div className="App">
-      <div className="footer">
-        <div className="footer-description">
-          POWERED BY MOONSTARDOM
-        </div>
-      </div>
-      <Routes />
-     
-    </div>
-  );
+      
+ </div>
+
 }
 
 export default App;
