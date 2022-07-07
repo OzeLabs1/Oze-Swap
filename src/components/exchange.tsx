115e95dbef005d6aed6b44336d0dcc6b21046507
Ajay Kumar <ajaythakur9131@gmail.com>
2022-07-07 15:20:45 +0530

    updates
    styling

--- src/components/exchange.tsx
+++ src/components/exchange.tsx
@@ -34,6 +34,7 @@ export const ExchangeView = (props: {}) => {
     <div className="App-Bar">
       <div className="App-Bar-left">
         <div className="App-logo" />
+        <h1>Oze Labs</h1>
       </div>
       <div className="App-Bar-right">
         <Button type="text" size="large" style={{ color: "#2abdd2" }}>
