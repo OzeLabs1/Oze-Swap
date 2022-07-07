6644dec3789df7ec567292eab1a5625e1a3e2706
Ajay Kumar <ajaythakur9131@gmail.com>
2022-07-07 15:31:24 +0530

    changes in header

--- src/components/exchange.tsx
+++ src/components/exchange.tsx
@@ -37,7 +37,7 @@ export const ExchangeView = (props: {}) => {
         <h1>Oze Labs</h1>
       </div>
       <div className="App-Bar-right">
-        <Button type="text" size="large" style={{ color: "#2abdd2" }}>
+        <Button type="text" size="large" style={{ color:"#FFF" }}>
           <a
             href={"https://dex.projectserum.com"}
             target="_blank"
@@ -62,7 +62,7 @@ export const ExchangeView = (props: {}) => {
               type="text"
               size="large"
               onClick={connected ? wallet.disconnect : wallet.connect}
-              style={{ color: "#2abdd2" }}
+              style={{ color: "#FFF" }}
             >
               Connect
             </Button>
